f8d2794e58e931ddcbd2f10dd5651bc8ef815e28
myaradesignstudio <myaradesignstudio@gmail.com>
2025-10-31 00:29:36 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = 'myara-cache-v1';
+const assets = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
