2750cff437564e4d93ef3e4c272195b9255e5ea9
myaradesignstudio <myaradesignstudio@gmail.com>
2025-10-31 01:28:39 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,10 @@
-const cacheName = 'myara-cache-v1';
-const assets = [
-  '/',
-  'index.html',
-  'manifest.json',
-  'icon-192.png',
-  'icon-512.png'
-];
+const cacheName = 'myara-cache-v2';
+const assets = ['index.html','manifest.json','/'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName)
-      .then(cache => cache.addAll(assets))
-  );
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
